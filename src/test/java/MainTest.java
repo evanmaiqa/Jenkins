8aaa3213038c606e2a02a3f7ddc78c5531fea854
Evan Mai <mai.evan@yahoo.com>
2023-02-19 01:36:14 -0800

    upd

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,8 +1,12 @@
 import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 
@@ -20,38 +24,43 @@ public class MainTest {
     }
 
     private static void setReport() throws IOException {
-        FileWriter fileWriter = new FileWriter("report.txt", true);
+        FileWriter fileWriter = new FileWriter("report.csv", true);
         fileWriter.write(getBrowserName() + "\n");
         fileWriter.close();
     }
 
-
     @Test
     public void firstTest() throws IOException {
-        if (!new File("report.txt").createNewFile()) {
-            FileWriter fileWriter = new FileWriter("report.txt", false);
+        if (!new File("report.csv").createNewFile()) {
+            FileWriter fileWriter = new FileWriter("report.csv", false);
             fileWriter.write("");
-//            fileWriter.close();
+            fileWriter.close();
         }
         for (String browser : browsersArr) {
             switch (browser) {
                 case "chrome":
                     System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-                    driver = new ChromeDriver();
+                    ChromeOptions optionsChrome = new ChromeOptions();
+                    optionsChrome.addArguments("--headless");
+                    driver = new ChromeDriver(optionsChrome);
                     break;
                 case "firefox":
                     System.setProperty("webdriver.gecko.driver", "geckodriver.exe");
-                    driver = new FirefoxDriver();
+                    FirefoxOptions optionsFirefox = new FirefoxOptions();
+                    optionsFirefox.setHeadless(true);
+                    driver = new FirefoxDriver(optionsFirefox);
                     break;
                 case "msedge":
                     System.setProperty("webdriver.edge.driver", "msedgedriver.exe");
-                    driver = new EdgeDriver();
+                    EdgeOptions optionsEdge = new EdgeOptions();
+                    optionsEdge.setCapability("UseChromium", true);
+         //           optionsEdge.
+                    driver = new EdgeDriver(optionsEdge);
                     break;
             }
+            driver.manage().window().setSize(new Dimension(1920, 1080));
             setReport();
-
             driver.quit();
         }
-
     }
 }
