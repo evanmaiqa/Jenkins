a463f4f178375c93a469ac90389c57e1ebd7602c
evanmaiqa <mai.evan@yahoo.com>
2023-02-19 02:13:24 -0800

    upd

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.safari.SafariOptions;
 import org.testng.annotations.Test;
 
 import java.io.File;
@@ -15,17 +17,18 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class MainTest {
-    private static final String[] browsersArr = new String[]{"chrome", "firefox", "msedge"};
+    private static final String osName = System.getProperty("os.name").toLowerCase();
+    private static final String[] browsersArr = new String[]{"chrome", "firefox", "msedge", "safari"};
     private static WebDriver driver;
 
     private static String getBrowserName() {
         Capabilities caps = ((RemoteWebDriver) driver).getCapabilities();
-        return caps.getBrowserName() + " " + caps.getVersion() + " " + caps.getPlatform().toString().toLowerCase();
+        return caps.getBrowserName() + ";" + caps.getVersion() + ";" + caps.getPlatform().toString().toLowerCase();
     }
 
     private static void setReport() throws IOException {
         FileWriter fileWriter = new FileWriter("report.csv", true);
-        fileWriter.write(getBrowserName() + "\n");
+        fileWriter.write(getBrowserName() + ";" + driver.manage().window().getSize() + "\n");
         fileWriter.close();
     }
 
@@ -36,31 +39,67 @@ public class MainTest {
             fileWriter.write("");
             fileWriter.close();
         }
-        for (String browser : browsersArr) {
-            switch (browser) {
-                case "chrome":
-                    System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-                    ChromeOptions optionsChrome = new ChromeOptions();
-                    optionsChrome.addArguments("--headless");
-                    driver = new ChromeDriver(optionsChrome);
-                    break;
-                case "firefox":
-                    System.setProperty("webdriver.gecko.driver", "geckodriver.exe");
-                    FirefoxOptions optionsFirefox = new FirefoxOptions();
-                    optionsFirefox.setHeadless(true);
-                    driver = new FirefoxDriver(optionsFirefox);
-                    break;
-                case "msedge":
-                    System.setProperty("webdriver.edge.driver", "msedgedriver.exe");
-                    EdgeOptions optionsEdge = new EdgeOptions();
-                    optionsEdge.setCapability("UseChromium", true);
-         //           optionsEdge.
-                    driver = new EdgeDriver(optionsEdge);
-                    break;
+
+        if (osName.contains("mac os x")) {
+            for (String browser : browsersArr) {
+                switch (browser) {
+                    case "chrome":
+                        System.setProperty("webdriver.chrome.driver", "chromedriver");
+                        ChromeOptions optionsChrome = new ChromeOptions();
+                        optionsChrome.addArguments("--headless");
+                        driver = new ChromeDriver(optionsChrome);
+                        break;
+                    case "firefox":
+                        System.setProperty("webdriver.gecko.driver", "geckodriver");
+                        FirefoxOptions optionsFirefox = new FirefoxOptions();
+                        optionsFirefox.setHeadless(true);
+                        driver = new FirefoxDriver(optionsFirefox);
+                        break;
+                    case "msedge":
+                        System.setProperty("webdriver.edge.driver", "msedgedriver");
+                        EdgeOptions optionsEdge = new EdgeOptions();
+                        optionsEdge.setCapability("UseChromium", true);
+//                        optionsEdge.addArguments("headless");
+                        driver = new EdgeDriver(optionsEdge);
+                        break;
+                    case "safari":
+                        driver = new SafariDriver();
+                        break;
+                }
+                driver.manage().window().setSize(new Dimension(1920, 1080));
+                setReport();
+                driver.quit();
             }
-            driver.manage().window().setSize(new Dimension(1920, 1080));
-            setReport();
-            driver.quit();
+        } else if (osName.contains("windows")) {
+            for (String browser : browsersArr) {
+                switch (browser) {
+                    case "chrome":
+                        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+                        ChromeOptions optionsChrome = new ChromeOptions();
+                        optionsChrome.addArguments("--headless");
+                        driver = new ChromeDriver(optionsChrome);
+                        break;
+                    case "firefox":
+                        System.setProperty("webdriver.gecko.driver", "geckodriver.exe");
+                        FirefoxOptions optionsFirefox = new FirefoxOptions();
+                        optionsFirefox.setHeadless(true);
+                        driver = new FirefoxDriver(optionsFirefox);
+                        break;
+                    case "msedge":
+                        System.setProperty("webdriver.edge.driver", "msedgedriver.exe");
+                        EdgeOptions optionsEdge = new EdgeOptions();
+                        optionsEdge.setCapability("UseChromium", true);
+                        //           optionsEdge.
+                        driver = new EdgeDriver(optionsEdge);
+                        break;
+                }
+                driver.manage().window().setSize(new Dimension(1920, 1080));
+                setReport();
+                driver.quit();
+            }
+        } else {
+            System.out.println("NULL");
         }
+
     }
 }
